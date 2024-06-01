d70e9e2fdaaa4da20828a5f53c400818d34ad1fe
HaithemGhattass <haithem.ghattas@esprit.tn>
2024-06-01 12:50:37 +0100

    feat: side bar

---
+++ app/(dashboard)/_components/organizationsSidebar.tsx
@@ -0,0 +1,8 @@
+"use client";
+export const OrganizationsSidebar = () => {
+    return (
+        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+            <h1>Organizations sidebar</h1>
+        </div>
+    );
+}
